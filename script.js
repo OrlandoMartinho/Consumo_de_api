7ac2f583dc67d422755e134159afc9acefd81a4b
OrlandoMartinho <srsaiombo@gmail.com>
2023-09-25 20:22:08 +0100

    Subindo tudo

---
+++ script.js
@@ -0,0 +1,20 @@
+function enviarImagem() {
+    var imagem = document.getElementById("imagem").files[0];
+    var formData = new FormData();
+    formData.append("imagem", imagem);
+
+    fetch("http://localhost:5000/prever", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        var resultado = document.getElementById("resultado");
+        resultado.innerHTML = "Classe predita: " + data.classe_predita + "<br>";
+        resultado.innerHTML += "Confianças:<br>";
+        for (var classe in data.confiancas) {
+            resultado.innerHTML += classe + ": " + data.confiancas[classe].toFixed(1) + "%<br>";
+        }
+    })
+    .catch(error => console.error("Erro:", error));
+}
